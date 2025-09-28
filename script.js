248e944e6c9ee753fd1146b2f0a2a506b7dabb44
dangarten <danielsmendanhaa@gmail.com>
2025-09-27 22:46:13 -0300

    calendar+background+changes

---
+++ script.js
@@ -0,0 +1,30 @@
+// Hover Sound Effect
+const hoversoundeffect = new Audio('audio/hoverse.mp3');
+    function playhoversoundeffect() {
+        hoversoundeffect.currentTime = 0; 
+        hoversoundeffect.play();
+    }
+// Background Music
+ const backgroundmusic = new Audio('music/april14th.mp3');
+    backgroundmusic.loop = true;
+        backgroundmusic.volume = 0.1;
+    function togglemusic() {
+        if (backgroundmusic.paused) {
+            backgroundmusic.play();
+            document.getElementById("buttonmusic").src = "images/icons/volumeon.png";
+        }else {
+            backgroundmusic.pause();
+            document.getElementById("buttonmusic").src = "images/icons/volumeoff.png";
+        }
+    }
+// Calendar 
+function calendar() {
+    const divcalendar = document.querySelector('.calendar');
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const weekdays = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    const weekday = weekdays[today.getDay()];
+    divcalendar.innerHTML = `<span>${day}/${month}</span> <span class="day">${weekday}</span>`;
+}
+calendar();
--- scriptaudio.js
+++
@@ -1,4 +0,0 @@
-var audio = $("#mySoundClip")[0];
-$("nav a").mouseenter(function() {
-  audio.play();
-});
